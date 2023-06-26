36e80c1e43ec2a40f5cf6b18a377981853851c79
mirza-mohibul-hasan <mirzamohibul618@gmail.com>
2023-06-26 20:22:24 +0600

    folder structure added

--- src/app/protfolio/page.jsx
+++ src/app/protfolio/page.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-const About = () => {
+
+const Protfolio = () => {
     return (
         <div>
-            About
+            Protfolio
         </div>
     );
 };
 
-export default About;
+export default Protfolio;
